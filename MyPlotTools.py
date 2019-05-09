e91ef3c7843195b30a16cbc7a3d9a9b528bb6c66
Pooya <pooya.bagheri@gmail.com>
2019-05-09 16:43:30 -0600

    Plot tools bug fix

--- MyPlotTools.py
+++ MyPlotTools.py
@@ -6,18 +6,18 @@ import numpy as np
 #class for plotting inside my analytic jupyter notebooks
 class Plot():
     def __init__(self,plotHeight=5,plotWidth=6,FromDay=1,ToDay=1):
-        plt.rcParams['figure.figsize'] = [plotHeight,plotWidth] #making the plots a bit larger than default size
+        plt.rcParams['figure.figsize'] = [plotWidth,plotHeight] #making the plots a bit larger than default size
         self.TimeAxisLimits(FromDay,ToDay)
     def TimeAxisLimits(self,FromDay,ToDay):
         self.FromDay=FromDay
         self.ToDay=ToDay
-    def Compare(self,predicted,actual,title=None,legends=['Predicted', 'Actual'],yAxisLabel='Power Loss (kW)'):
+    def Compare(self,predicted,actual,title=None,legends=['Predicted', 'Actual'],yAxisLabel='Power Loss (kW)',legendloc='upper left'):
         Times=np.linspace(self.FromDay-1,self.ToDay,predicted.shape[0])
         plt.plot(Times,predicted)
         plt.plot(Times,actual)
         if title:
             plt.title(title)
-        plt.xlabel('Time (per day)')        
+        plt.xlabel('Measurement Time (per day)')        
         plt.ylabel(yAxisLabel)
-        plt.legend(legends, loc='upper left')
+        plt.legend(legends, loc=legendloc)
         plt.show()    
