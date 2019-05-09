f7be6aeafa692f21dd76c8c4e23c07eb95ab649f
Pooya <pooya.bagheri@gmail.com>
2019-05-09 16:45:17 -0600

    general bug fixes

--- LoadingDataClass.py
+++ LoadingDataClass.py
@@ -2,24 +2,25 @@
 import os 
 import pandas as pd
 from sqlalchemy import create_engine
+
+ResultsDBfile='SimResultsIEEE123NodesCase2.db'
+
 # a class is defined for loading data and covering the SQL and pivoting procedure:
-class MLinputData:
-    DB=None #SQL database only needs to be initialized for the whole class once 
+class MLinputData: 
+    #initialize the API with the database:
+    DB = create_engine('sqlite:///'+os.path.abspath('')+'//'+ResultsDBfile, echo=False)     
+    
     def __init__(self,FromDay,ToDay):
-        if self.DB is None: #Ensures to initialize SQL database as soon as the first instance of class is created
-            DBfilePath=os.path.abspath('')+'//'+'SimResultsIEEE123NodesCase2.db'
-            self.DB = create_engine('sqlite:///'+DBfilePath, echo=False)   
         RawVoltages=pd.read_sql_query('''
         select Instants.InstantID,Voltages.NodeID,Voltages.Vmag
         from Instants join Voltages on Instants.InstantID=Voltages.InstantID
         where Instants.Day>=%d and Instants.Day<=%d''' % (FromDay,ToDay),con=self.DB)
         Voltages=RawVoltages.pivot(index='InstantID', columns='NodeID', values='Vmag')
         self.x=Voltages.values
-        """
         Ploss=pd.read_sql_query('''
         select Losses.Ploss
         from Instants join Losses on Instants.InstantID=Losses.InstantID
-        where Instants.Day>=%d and Instants.Day<=%d''',% (FromDay,ToDay),con=self.DB)
+        where Instants.Day>=%d and Instants.Day<=%d''' % (FromDay,ToDay),con=self.DB)
         self.y=Ploss.values
-        """
+     
         
